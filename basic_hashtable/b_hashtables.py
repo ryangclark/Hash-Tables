74a11ff23912fcf468409863510810c63e2ddd12
Clark <rclark010@gmail.com>
2019-07-08 15:56:02 -0400

    Completes Day 1 tasks

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -15,7 +15,9 @@ class Pair:
 # '''
 class BasicHashTable:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity # Max size
+        self.count = 0  # Current size being used
+        self.storage = [None] * capacity
 
 
 # '''
@@ -23,7 +25,46 @@ class BasicHashTable:
 # Research and implement the djb2 hash function
 # '''
 def hash(string, max):
-    pass
+	# a good number to start with, especially if `string` is short
+    hash = 5381
+
+    for i in string:
+    	# `(hash << 5) + hash` == `hash * 33`
+    	hash = ((hash << 5) + hash) ^ ord(i)
+
+    # print('hash:', hash, len(str(hash)))
+    # print('hash &:', hash & 0xFFFFFFFF, len(str(hash & 0xFFFFFFFF)))
+    # print('hash % max', hash % max, len(str(hash % max)))
+
+    return hash % max
+
+
+	# const MAGIC_CONSTANT = 5381;
+
+	# const djb2a = string => {
+	# 	let hash = MAGIC_CONSTANT;
+
+	# 	for (let i = 0; i < string.length; i++) {
+	# 		// Equivalent to: `hash * 33 ^ string.charCodeAt(i)`
+	# 		hash = ((hash << 5) + hash) ^ string.charCodeAt(i);
+	# 	}
+
+	# 	// Convert it to an unsigned 32-bit integer
+	# 	return hash >>> 0;
+	# };
+
+
+	# def hash_djb2(s):                                                                                                                                
+	#     hash = 5381
+	#     for x in s:
+	#         hash = (( hash << 5) + hash) + ord(x)
+	# 	return hash & 0xFFFFFFFF
+
+	# def djb2(base, word):
+	#     ''' Hash a word using the djb2 algorithm with the specified base. '''
+	#     for c in word:
+	#         base = ((base << 5) + base + ord(c)) & 0xffffffff
+	# 	return base
 
 
 # '''
@@ -32,7 +73,19 @@ def hash(string, max):
 # If you are overwriting a value with a different key, print a warning.
 # '''
 def hash_table_insert(hash_table, key, value):
-    pass
+    # hash the key
+    # check HT index for hashed index
+    # if not None and HT[hashed index][0] != key
+    # assign value to HT index
+
+    hashed_index = hash(key, hash_table.capacity)
+
+    if hash_table.storage[hashed_index] and hash_table.storage[hashed_index][0] != key:
+    	print(f'Overwriting key {hash_table.storage[hashed_index][0]} with {key}!')
+
+    hash_table.storage[hashed_index] = (key, value)
+
+    return
 
 
 # '''
@@ -41,7 +94,14 @@ def hash_table_insert(hash_table, key, value):
 # If you try to remove a value that isn't there, print a warning.
 # '''
 def hash_table_remove(hash_table, key):
-    pass
+    hashed_index = hash(key, hash_table.capacity)
+
+    if not hash_table.storage[hashed_index]:
+    	return print(f'Key {key} not found!')
+
+    hash_table.storage[hashed_index] = None
+
+    return
 
 
 # '''
@@ -50,13 +110,22 @@ def hash_table_remove(hash_table, key):
 # Should return None if the key is not found.
 # '''
 def hash_table_retrieve(hash_table, key):
-    pass
+    hashed_index = hash(key, hash_table.capacity)
+
+    retrieved = hash_table.storage[hashed_index]
+
+    if retrieved:
+    	return retrieved[1]
+    else:
+    	return retrieved
 
 
 def Testing():
     ht = BasicHashTable(16)
 
     hash_table_insert(ht, "line", "Here today...\n")
+
+    print(hash_table_retrieve(ht, "line"))
 
     hash_table_remove(ht, "line")
 
